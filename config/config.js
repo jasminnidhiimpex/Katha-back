2ec24db0a82adb4c1284932b4f4519c561cfccb9
maitri <maitri.nidhiimpex@gmail.com>
2025-06-26 17:32:43 +0530

    fix

--- config/config.js
+++ config/config.js
@@ -1,18 +1,20 @@
 require('dotenv').config();
-
+ 
 module.exports = {
   development: {
     username: process.env.DB_USER,  
     password: process.env.DB_PASSWORD, 
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST ,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql"
   },
   test: {
-    username: process.env.DB_USER ,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql"
   },
   production: {
@@ -20,6 +22,12 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true
+      }
+    }
   }
-};
+}
