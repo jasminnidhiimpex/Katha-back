d82cf6f954236ecfcda6109a6a573db0af2ffc1d
maitri <maitri.nidhiimpex@gmail.com>
2025-06-26 17:37:31 +0530

    fix

--- config/config.js
+++ config/config.js
@@ -18,16 +18,17 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: true
-      }
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // <-- allow self-signed certs
     }
   }
+}
 }
