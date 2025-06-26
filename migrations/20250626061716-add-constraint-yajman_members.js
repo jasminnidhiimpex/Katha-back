b4a502b85d1e144dad54a3fbe0888f598c8335e3
maitri <maitri.nidhiimpex@gmail.com>
2025-06-26 18:06:37 +0530

    FIX

--- migrations/20250626061716-add-constraint-yajman_members.js
+++ migrations/20250626061716-add-constraint-yajman_members.js
@@ -4,6 +4,10 @@ module.exports = {
       type: Sequelize.STRING(255), // converts to VARCHAR(255)
       allowNull: true,
     });
+    await queryInterface.changeColumn('yajman_members', 'gender', {
+      type: Sequelize.STRING(255), // converts to VARCHAR(255)
+      allowNull: true,
+    });
     await queryInterface.addConstraint('yajman_members', {
       fields: ['full_name', 'age', 'aadhaar', 'mobile', 'gender'],
       type: 'unique',
